71c9ce0e11427959b87ce494935e2d3202314654
hachi-02 <dungntps40706@gmail.com>
2024-11-29 19:29:08 +0700

    up

--- routes/sanpham.js
+++ routes/sanpham.js
@@ -19,7 +19,8 @@ router.post("/add",async function(req,res) {
     try {
       const {tensp,gia,size}=req.body;
       const newItem={tensp,gia,size};
-      await sanpham.create(newItem);
+      const saveSP=await sanpham.create(newItem);
+      const finalSP= await sanpham.findById(saveSP._id).populate('size');
       res.status(200).json({status:true,message:"thêm thành công"});
     } catch (error) {
       res.status(400).json({status:false,message:"có lỗi xảy ra"});
